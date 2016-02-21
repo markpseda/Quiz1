8b89bc44b03e3223d4511021f4f26fc8e098c1b2
Mark Seda <markpseda@gmail.com>
2016-02-21 12:56:39 -0500

    Final Version

--- Quiz1Q1/src/mainPackage/Main.java
+++ Quiz1Q1/src/mainPackage/Main.java
@@ -1,22 +1,49 @@
 package mainPackage;
 import java.util.Scanner;
 
+/*This program serves to calculate the passer rating of a given quarterback.
+ * The formula used is the standard used by the NFL and CFL found at
+ * https://en.wikipedia.org/wiki/Passer_rating
+ * The program prompts the user for the QB's game statistics, one by one, and then
+ * outputs their rating.
+ * */
+
 public class Main {
 	
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);				//initialize scanner
 		
-		System.out.println("Enter the QB's touchdowns. ");
-		int touchDowns = input.nextInt();
+		System.out.println("Enter the QB's touchdowns. ");	//user inputs touchdowns
+		double touchDowns = input.nextDouble();
+		
+		System.out.println("Enter the QB's total yards. ");	//user inputs yards
+		double yards = input.nextDouble();
+		
+		System.out.println("Enter the QB's interceptions. ");//user inputs interceptions
+		double interceptions = input.nextDouble();
+		
+		System.out.println("Enter the QB's completions. ");	//user inputs completions
+		double completions = input.nextDouble();
+		
+		System.out.println("Enter the QB's passes attempted. ");//user inputs attempts
+		double attempts = input.nextDouble();
 		
 		input.close();
 		
-		System.out.println(calculateRating(touchDowns));
+		System.out.print("This QB's passer rating is: "); 
+		System.out.printf("%.2f",calculateRating(touchDowns, yards, //2 decimal place accuracy
+				interceptions, completions, attempts));
 	}
 	
-	public static int calculateRating(int touchDowns){
-		System.out.println("eggs");
-		return 5;
+	private static double calculateRating(double touchDowns, double yards,
+			double interceptions, double completions, double attempts){
+		
+		double a = ((completions/attempts - .3)*5); 		//formula given online
+		double b = ((yards/attempts - 3)*.25);
+		double c = ((touchDowns/attempts)*20);
+		double d = (2.375 - (interceptions/attempts)*25);
+
+		return (((a+b+c+d)/6)*100);
 	}
 
 }
